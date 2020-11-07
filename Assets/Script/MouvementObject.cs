bc66b68bd9435adc17eccccf22291b69f1331dff
Aldric-Ducreux <neogeekmo@live.fr>
2020-11-07 20:45:01 +0100

    add rotation + scale (lean touch) + TextmeshPro

---
+++ Assets/Script/MouvementObject.cs
@@ -0,0 +1,23 @@
+﻿using Lean.Touch;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouvementObject : MonoBehaviour
+{
+    [SerializeField] private GameObject visualTarget;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        visualTarget = transform.GetChild(0).gameObject;
+        visualTarget.AddComponent<LeanPinchScale>();
+        LeanTwistRotateAxis script = visualTarget.AddComponent<LeanTwistRotateAxis>();
+
+        script.Axis.x = -1;
+        script.Axis.y = -1;
+
+    }
+}
